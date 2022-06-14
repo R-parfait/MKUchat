10d0c359dd2ddf07000547d7538704821d5371ea
PARFAIT mbaitorndia roger <rogerparfaitmbaitondia@gmail.com>
2022-06-14 11:59:05 +0300

    welcome page design

--- MKUchat/home/urls.py
+++ MKUchat/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path("", views.welcomepage, name="welcomepage"),
     path("login", views.loginpage, name="loginpage"),
+    path("createaccount", views.createaccount, name="createaccount"),
     path("logout", views.logoutpage, name="logoutpage"),
 ]
--- MKUchat/home/views.py
+++ MKUchat/home/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def welcomepage(request):
-    return HttpResponse("Welcome to MKUchat!")
+    return render(request, "home/Welcomepage.html")
 
 def loginpage(request):
     return HttpResponse("Login Form")
+
+def createaccount(request):
+    return HttpResponse("we shoud have the form to create an account here")
 
 def logoutpage(request):
     return HttpResponse("You have succesfully logged out")
